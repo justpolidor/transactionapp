a92180673a072bfdca6ac5af036a423b156dcb19
Justin <justpolidor@gmail.com>
2017-10-07 19:32:26 +0200

    Mock object store setup

---
+++ domain/src/main/java/it/n26/domain/setup/RepositorySetup.java
@@ -0,0 +1,10 @@
+package it.n26.domain.setup;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("it.n26.domain.repository")
+@Configuration
+public class RepositorySetup {
+
+}
