2d46d3aa2f90829d799415d24e65ef1e49f7f7b8
Justin <justpolidor@gmail.com>
2017-10-08 16:13:24 +0200

    Removed unused annotation

--- service/src/main/java/it/n26/service/TransactionsApplication.java
+++ service/src/main/java/it/n26/service/TransactionsApplication.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @SpringBootApplication
 @Import(value = {RepositorySetup.class})
-@EnableScheduling
 public class TransactionsApplication {
 
 	public static void main(String[] args) {
